c32d3f790d157e1c994c5a0e3828192825447a1f
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-28 13:19:20 -0600

    add relationship model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# User.create!([
+#   { name: “Van” },
+#   { name: “Ann” },
+#   { name: “Steven” },
+# ])
+
+Relationship.create(leader_id: 1, follower_id: 2)
+Relationship.create(leader_id: 1, follower_id: 3)
+Relationship.create(leader_id: 2, follower_id: 1)
+Relationship.create(leader_id: 2, follower_id: 3)
+Relationship.create(leader_id: 3, follower_id: 1)
+Relationship.create(leader_id: 3, follower_id: 2)
