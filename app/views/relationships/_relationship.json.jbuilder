c26c9958482e285f774146fc94e50e206248e929
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-28 13:40:59 -0600

    add relationship jbuilder

---
+++ app/views/relationships/_relationship.json.jbuilder
@@ -0,0 +1,5 @@
+json.id relationship.id
+json.leader_id relationship.leader_id
+json.follower_id relationship.follower_id
+json.created_at relationship.created_at
+json.updated_at relationship.updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@
 #   { name: “Steven” },
 # ])
 
-Relationship.create(leader_id: 1, follower_id: 2)
-Relationship.create(leader_id: 1, follower_id: 3)
-Relationship.create(leader_id: 2, follower_id: 1)
-Relationship.create(leader_id: 2, follower_id: 3)
-Relationship.create(leader_id: 3, follower_id: 1)
-Relationship.create(leader_id: 3, follower_id: 2)
+# Relationship.create(leader_id: 1, follower_id: 2)
+# Relationship.create(leader_id: 1, follower_id: 3)
+# Relationship.create(leader_id: 2, follower_id: 1)
+# Relationship.create(leader_id: 2, follower_id: 3)
+# Relationship.create(leader_id: 3, follower_id: 1)
+# Relationship.create(leader_id: 3, follower_id: 2)
